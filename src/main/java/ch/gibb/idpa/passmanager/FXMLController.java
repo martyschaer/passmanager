cb4471072240f4865522a52e5486dc0255fb706c
Jean-Rémy <jean-remy@gmx.ch>
2016-11-01 21:07:55 +0100

    - Basic maven project - Native and Java Web Start package (.html file has a bug)

---
+++ src/main/java/ch/gibb/idpa/passmanager/FXMLController.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2016 Gibb
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package ch.gibb.idpa.passmanager;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * Main controller for FX example.
+ *
+ * @author Jean-Rémy Buchs <jean-remy@gmx.ch>
+ */
+public class FXMLController implements Initializable {
+
+	@FXML
+	private Label label;
+
+	@FXML
+	public void handleButtonAction(ActionEvent event) {
+		System.out.println("You clicked me!");
+		label.setText("Hello World!");
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO
+	}
+}
